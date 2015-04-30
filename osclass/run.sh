8e881de2ef9889bb9f756cd99643e3d777e640ff
XaviOutside <xavioutside@hotmail.com>
2015-05-01 00:00:00 +0200

    Adding new file that setup Osclass container.

---
+++ osclass/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
+
+# I don't know why but it seems that they don't get the permission
+mv /var/www/html /var/www/html.old && mv /var/www/html.old /var/www/html
+
+service apache2 start
