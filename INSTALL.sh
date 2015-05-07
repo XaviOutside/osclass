2639e1bbeb0cbccb815a4ab434a7da28abd9b146
XaviOutside <xavioutside@hotmail.com>
2015-05-07 09:25:38 +0200

    Adding new container for mailing. Postfix server.

--- INSTALL.sh
+++ INSTALL.sh
@@ -34,11 +34,21 @@ cd ${TOPDIR}/osclass && docker build --force-rm --rm -t osclass .
 print_banner "CREATING OSCLASS CONTAINER...[completed]"
 sleep 1;
 
+print_banner "CREATING POSTFIX CONTAINER...[starting]"
+cd ${TOPDIR}/postfix && docker build --force-rm --rm -t postfix .
+print_banner "CREATING POSTFIX CONTAINER...[completed]"
+sleep 1;
+
 print_banner "RUNNING MYSQL CONTAINTER...[starting]"
 docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456789 --name mysql mysql5.6
 print_banner "RUNNING MYSQL CONTAINTER...[completed]"
 sleep 1;
 
 print_banner "RUNNING OSCLASS CONTAINTER...[starting]"
-docker run -d -p 80:80 --name osclass --link mysql:mysql osclass
+docker run -d -p 80:80 -e DOMAIN=www.example.org --name osclass --link mysql:mysql osclass
 print_banner "RUNNING OSCLASS CONTAINTER...[completed]"
+sleep 1;
+
+print_banner "RUNNING POSTFIX CONTAINTER...[starting]"
+docker run -d -p 25:25 -e DOMAIN=www.example.org --name postfix postfix
+print_banner "RUNNING POSTFIX CONTAINTER...[completed]"
