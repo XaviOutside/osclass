da8930af33ccc92fbdecff617243c6abaedf1483
XaviOutside <xavioutside@hotmail.com>
2015-05-12 11:18:32 +0200

    Testing new volume docker containers for mysql and osclass.

--- INSTALL.sh
+++ INSTALL.sh
@@ -39,13 +39,23 @@ cd ${TOPDIR}/postfix && docker build -t postfix .
 print_banner "CREATING POSTFIX CONTAINER...[completed]"
 sleep 1;
 
+print_banner "CREATING MYSQL VOLUME CONTAINER...[starting]"
+cd ${TOPDIR}/postfix && docker create -v /var/lib/mysql --name mysql_volume mysql5.6 /bin/true
+print_banner "CREATING MYSQL VOLUME CONTAINER...[completed]"
+sleep 1;
+
+print_banner "CREATING OSCLASS VOLUME CONTAINER...[starting]"
+cd ${TOPDIR}/postfix && docker create -v /var/www/html --name osclass_html_volume osclass /bin/true
+print_banner "CREATING OSCLASS VOLUME CONTAINER...[completed]"
+sleep 1;
+
 print_banner "RUNNING MYSQL CONTAINTER...[starting]"
-docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456789 --name mysql mysql5.6
+docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456789 --volumes-from mysql_volume --name mysql mysql5.6
 print_banner "RUNNING MYSQL CONTAINTER...[completed]"
 sleep 1;
 
 print_banner "RUNNING OSCLASS CONTAINTER...[starting]"
-docker run -d -p 80:80 -e DOMAIN=example.org --name osclass --link mysql:mysql osclass
+docker run -d -p 80:80 -e DOMAIN=example.org --volumes-from osclass_html_volume --name osclass --link mysql:mysql osclass
 print_banner "RUNNING OSCLASS CONTAINTER...[completed]"
 sleep 1;
 
