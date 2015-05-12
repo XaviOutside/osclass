c28a64bd33cfc07d4151b2409ec6cb9fa1dc680e
XaviOutside <xavioutside@hotmail.com>
2015-05-12 17:23:21 +0200

    Adding backup directory into osclass container in order to save backup there.

--- INSTALL.sh
+++ INSTALL.sh
@@ -40,13 +40,18 @@ cd ${TOPDIR} && docker create -v /var/www/html --name osclass_html_volume osclas
 print_banner "CREATING OSCLASS VOLUME CONTAINER...[completed]"
 sleep 1;
 
+print_banner "CREATING BACKUP VOLUME CONTAINER...[starting]"
+cd ${TOPDIR} && docker create -v /backup --name backup_volume osclass /bin/true
+print_banner "CREATING BACKUP VOLUME CONTAINER...[completed]"
+sleep 1;
+
 print_banner "RUNNING MYSQL CONTAINTER...[starting]"
 docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456789 --volumes-from mysql_volume --name mysql mysql:5.6
 print_banner "RUNNING MYSQL CONTAINTER...[completed]"
 sleep 1;
 
 print_banner "RUNNING OSCLASS CONTAINTER...[starting]"
-docker run -d -p 80:80 -e DOMAIN=example.org --volumes-from osclass_html_volume --name osclass --link mysql:mysql osclass
+docker run -d -p 80:80 -e DOMAIN=example.org --volumes-from backup_volume --volumes-from osclass_html_volume --name osclass --link mysql:mysql osclass
 print_banner "RUNNING OSCLASS CONTAINTER...[completed]"
 sleep 1;
 
